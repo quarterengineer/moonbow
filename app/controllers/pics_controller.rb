7b51ad0f3080364b727fa7e1e8d4bac57b843537
dogukan demirci <>
2019-04-30 20:55:25 +0300

    Kullanicilar postlarla eslestirildi

---
+++ app/controllers/pics_controller.rb
@@ -0,0 +1,51 @@
+class PicsController < ApplicationController
+    
+    before_action :find_pic, only: [:edit, :update, :show, :destroy]
+    skip_before_action :verify_authenticity_token, :only => [:destroy]
+    
+    def index
+        @pics = Pic.all.order("created_at DESC")
+    end
+    
+    def show 
+    end
+
+    def new
+        @pic = current_user.pics.build
+    end
+
+    def create
+        @pic = current_user.pics.build(pic_params)
+        if @pic.save
+            redirect_to @pic, notice: "Yess! It was posted!"
+        else
+            render "new"
+        end
+            
+    end
+    def edit
+    end
+    def update
+        if @pic.update(pic_params)
+            redirect_to @pic, notice: "Congrats! Pic was update"
+        else
+            render 'edit'
+        end
+    end
+    def destroy
+        @pic.destroy
+        redirect_to root_path
+    end
+
+
+    private
+    
+    def pic_params
+        params.require(:pic).permit(:title, :description)
+    end
+
+    def find_pic
+        @pic = Pic.find(params[:id])
+    end
+    
+end
